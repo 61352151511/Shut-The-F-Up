10d8ceddbde345d29f11e4df13c137aca41c0a45
61352151511 <cassboys@hotmail.com>
2016-05-18 12:31:05 -0300

    1.9.4 Update + New Goodies

--- src/main/java/com/sixonethree/stfu/handler/ConfigurationHandler.java
+++ src/main/java/com/sixonethree/stfu/handler/ConfigurationHandler.java
@@ -11,7 +11,26 @@ import com.sixonethree.stfu.Reference;
 public class ConfigurationHandler {
 	public static Configuration configuration;
 	
-	private static float volume = 0;
+	private static boolean shutUpAmbient = false;
+	private static boolean shutUpBlocks = false;
+	private static boolean shutUpHostile = false;
+	private static boolean shutUpMaster = false;
+	private static boolean shutUpMusic = false;
+	private static boolean shutUpNeutral = false;
+	private static boolean shutUpPlayers = false;
+	private static boolean shutUpRecords = false;
+	private static boolean shutUpVoice = false;
+	private static boolean shutUpWeather = false;
+	private static float volumeAmbient = 1.0F;
+	private static float volumeBlocks = 1.0F;
+	private static float volumeHostile = 1.0F;
+	private static float volumeMaster = 1.0F;
+	private static float volumeMusic = 0.0F;
+	private static float volumeNeutral = 1.0F;
+	private static float volumePlayers = 1.0F;
+	private static float volumeRecords = 1.0F;
+	private static float volumeVoice = 1.0F;
+	private static float volumeWeather = 1.0F;
 	
 	public static void init(File configFile) {
 		if (configuration == null) {
@@ -27,12 +46,109 @@ public class ConfigurationHandler {
 	}
 	
 	private static void loadConfiguration() {
-		volume = configuration.getFloat("Music Volume", Configuration.CATEGORY_GENERAL, 0F, 0F, 1F, "Volume for music to be set to on launch.");
+		shutUpAmbient = configuration.getBoolean("Shut Up Ambient", "Ambient", false, "");
+		volumeAmbient = configuration.getFloat("Ambient Volume", "Ambient", 1.0F, 0.0F, 1.0F, "Volume for ambient to be stuck at.");
+		shutUpBlocks = configuration.getBoolean("Shut Up Blocks", "Blocks", false, "");
+		volumeBlocks = configuration.getFloat("Blocks Volume", "Blocks", 1.0F, 0.0F, 1.0F, "Volume for blocks to be stuck at.");
+		shutUpHostile = configuration.getBoolean("Shut Up Hostile", "Hostile", false, "");
+		volumeHostile = configuration.getFloat("Hostile Volume", "Hostile", 1.0F, 0.0F, 1.0F, "Volume for hostile to be stuck at.");
+		shutUpMaster = configuration.getBoolean("Shut Up Master", "Master", false, "");
+		volumeMaster = configuration.getFloat("Master Volume", "Master", 1.0F, 0.0F, 1.0F, "Volume for master to be stuck at.");
+		shutUpMusic = configuration.getBoolean("Shut Up Music", "Music", true, "");
+		volumeMusic = configuration.getFloat("Music Volume", "Music", 0.0F, 0.0F, 1.0F, "Volume for music to be stuck at.");
+		shutUpNeutral = configuration.getBoolean("Shut Up Neutral", "Neutral", false, "");
+		volumeNeutral = configuration.getFloat("Neutral Volume", "Neutral", 1.0F, 0.0F, 1.0F, "Volume for neutral to be stuck at.");
+		shutUpPlayers = configuration.getBoolean("Shut Up Players", "Players", false, "");
+		volumePlayers = configuration.getFloat("Players Volume", "Players", 1.0F, 0.0F, 1.0F, "Volume for players to be stuck at.");
+		shutUpRecords = configuration.getBoolean("Shut Up Records", "Records", false, "");
+		volumeRecords = configuration.getFloat("Records Volume", "Records", 1.0F, 0.0F, 1.0F, "Volume for records to be stuck at.");
+		shutUpVoice = configuration.getBoolean("Shut Up Voice", "Voice", false, "");
+		volumeVoice = configuration.getFloat("Voice Volume", "Voice", 1.0F, 0.0F, 1.0F, "Volume for voice to be stuck at.");
+		shutUpWeather = configuration.getBoolean("Shut Up Weather", "Weather", false, "");
+		volumeWeather = configuration.getFloat("Weather Volume", "Weather", 1.0F, 0.0F, 1.0F, "Volume for weather to be stuck at.");
 		
 		if (configuration.hasChanged()) {
 			configuration.save();
 		}
 	}
-	
-	public static float getVolume() { return volume; }
+
+	public static boolean isShutUpAmbient() {
+		return shutUpAmbient;
+	}
+
+	public static boolean isShutUpBlocks() {
+		return shutUpBlocks;
+	}
+
+	public static boolean isShutUpHostile() {
+		return shutUpHostile;
+	}
+
+	public static boolean isShutUpMaster() {
+		return shutUpMaster;
+	}
+
+	public static boolean isShutUpMusic() {
+		return shutUpMusic;
+	}
+
+	public static boolean isShutUpNeutral() {
+		return shutUpNeutral;
+	}
+
+	public static boolean isShutUpPlayers() {
+		return shutUpPlayers;
+	}
+
+	public static boolean isShutUpRecords() {
+		return shutUpRecords;
+	}
+
+	public static boolean isShutUpVoice() {
+		return shutUpVoice;
+	}
+
+	public static boolean isShutUpWeather() {
+		return shutUpWeather;
+	}
+
+	public static float getVolumeAmbient() {
+		return volumeAmbient;
+	}
+
+	public static float getVolumeBlocks() {
+		return volumeBlocks;
+	}
+
+	public static float getVolumeHostile() {
+		return volumeHostile;
+	}
+
+	public static float getVolumeMaster() {
+		return volumeMaster;
+	}
+
+	public static float getVolumeMusic() {
+		return volumeMusic;
+	}
+
+	public static float getVolumeNeutral() {
+		return volumeNeutral;
+	}
+
+	public static float getVolumePlayers() {
+		return volumePlayers;
+	}
+
+	public static float getVolumeRecords() {
+		return volumeRecords;
+	}
+
+	public static float getVolumeVoice() {
+		return volumeVoice;
+	}
+
+	public static float getVolumeWeather() {
+		return volumeWeather;
+	}
 }
--- src/main/java/com/sixonethree/stfu/handler/TickHandler.java
+++ src/main/java/com/sixonethree/stfu/handler/TickHandler.java
@@ -14,9 +14,66 @@ public class TickHandler {
 		int curSecond = Calendar.getInstance().get(Calendar.SECOND);
 		if (curSecond != lastSecond) {
 			lastSecond = curSecond;
-			float curVolume = Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.MUSIC);
-			if (curVolume != ConfigurationHandler.getVolume()) {
-				Minecraft.getMinecraft().gameSettings.setSoundLevel(SoundCategory.MUSIC, ConfigurationHandler.getVolume());
+			float curVolume;
+			if (ConfigurationHandler.isShutUpAmbient()) {
+				curVolume = Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.AMBIENT);
+				if (curVolume != ConfigurationHandler.getVolumeAmbient()) {
+					Minecraft.getMinecraft().gameSettings.setSoundLevel(SoundCategory.AMBIENT, ConfigurationHandler.getVolumeAmbient());
+				}
+			}
+			if (ConfigurationHandler.isShutUpBlocks()) {
+				curVolume = Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.BLOCKS);
+				if (curVolume != ConfigurationHandler.getVolumeBlocks()) {
+					Minecraft.getMinecraft().gameSettings.setSoundLevel(SoundCategory.BLOCKS, ConfigurationHandler.getVolumeBlocks());
+				}
+			}
+			if (ConfigurationHandler.isShutUpHostile()) {
+				curVolume = Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.HOSTILE);
+				if (curVolume != ConfigurationHandler.getVolumeHostile()) {
+					Minecraft.getMinecraft().gameSettings.setSoundLevel(SoundCategory.HOSTILE, ConfigurationHandler.getVolumeHostile());
+				}
+			}
+			if (ConfigurationHandler.isShutUpMaster()) {
+				curVolume = Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.MASTER);
+				if (curVolume != ConfigurationHandler.getVolumeMaster()) {
+					Minecraft.getMinecraft().gameSettings.setSoundLevel(SoundCategory.MASTER, ConfigurationHandler.getVolumeMaster());
+				}
+			}
+			if (ConfigurationHandler.isShutUpMusic()) {
+				curVolume = Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.MUSIC);
+				if (curVolume != ConfigurationHandler.getVolumeMusic()) {
+					Minecraft.getMinecraft().gameSettings.setSoundLevel(SoundCategory.MUSIC, ConfigurationHandler.getVolumeMusic());
+				}
+			}
+			if (ConfigurationHandler.isShutUpNeutral()) {
+				curVolume = Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.NEUTRAL);
+				if (curVolume != ConfigurationHandler.getVolumeNeutral()) {
+					Minecraft.getMinecraft().gameSettings.setSoundLevel(SoundCategory.NEUTRAL, ConfigurationHandler.getVolumeNeutral());
+				}
+			}
+			if (ConfigurationHandler.isShutUpPlayers()) {
+				curVolume = Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.PLAYERS);
+				if (curVolume != ConfigurationHandler.getVolumePlayers()) {
+					Minecraft.getMinecraft().gameSettings.setSoundLevel(SoundCategory.PLAYERS, ConfigurationHandler.getVolumePlayers());
+				}
+			}
+			if (ConfigurationHandler.isShutUpRecords()) {
+				curVolume = Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.RECORDS);
+				if (curVolume != ConfigurationHandler.getVolumeRecords()) {
+					Minecraft.getMinecraft().gameSettings.setSoundLevel(SoundCategory.RECORDS, ConfigurationHandler.getVolumeRecords());
+				}
+			}
+			if (ConfigurationHandler.isShutUpVoice()) {
+				curVolume = Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.VOICE);
+				if (curVolume != ConfigurationHandler.getVolumeVoice()) {
+					Minecraft.getMinecraft().gameSettings.setSoundLevel(SoundCategory.VOICE, ConfigurationHandler.getVolumeVoice());
+				}
+			}
+			if (ConfigurationHandler.isShutUpWeather()) {
+				curVolume = Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.WEATHER);
+				if (curVolume != ConfigurationHandler.getVolumeWeather()) {
+					Minecraft.getMinecraft().gameSettings.setSoundLevel(SoundCategory.WEATHER, ConfigurationHandler.getVolumeWeather());
+				}
 			}
 		}
 	}
